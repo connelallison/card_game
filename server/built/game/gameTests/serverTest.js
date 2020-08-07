189007d62589b77aeb3b60c4148453bf5beff2b2
unknown <44365756+connelallison@users.noreply.github.com>
2020-08-07 10:06:29 +0100

    remove built from repo

--- server/built/game/gameTests/serverTest.js
+++
@@ -1,8 +0,0 @@
-var GamePlayer = require('../GamePlayer');
-var Game = require('../Game');
-var Cards = require('../CardLib');
-var Card = require('../Card');
-var Minion = require('../Minion');
-var Spell = require('../Spell');
-var _a = require('../Deck'), Deck = _a.Deck, deck1 = _a.deck1, deck2 = _a.deck2;
-var testGame = new Game(deck1, deck2, false, true);
